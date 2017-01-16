5da06de6cb15eab89a74215e8f6d0b1015d34c76
jean-raymond <jpierre@live.com>
2017-01-15 21:11:58 -0800

    dynamic control examples
    Creating Dynamic Data Entry User Interfaces
    https://msdn.microsoft.com/en-us/library/aa479330.aspx

---
+++ latus/latus/dyncontrol.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="dyncontrol.aspx.cs" Inherits="latus.dyncontrol" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Placeholder id="dynamicControls" runat="server" ></asp:Placeholder>
+    </div>
+    <div>
+        <asp:Placeholder id="placeHolder1" runat="server" ></asp:Placeholder>
+    </div>
+    </form>
+</body>
+</html>
---
+++ latus/latus/dyncontrol.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace latus
+{
+    public partial class dyncontrol : System.Web.UI.Page
+    {
+        Label lbl = new Label();
+        TextBox tb = new TextBox();
+        Button btn = new Button();
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            // Add dynamic controls to the init portion of the web page
+            Page.Controls.Add(lbl);
+
+            // When adding a control, set the ID property
+
+            placeHolder1.Controls.Add(tb);
+            tb.ID = "dynTextBox";
+
+            placeHolder1.Controls.Add(btn);
+            btn.ID = "dynButton1";
+
+            placeHolder1.Controls.Add(btn);
+            btn.ID = "dynButton2";
+
+            UserControl dq = new UserControl();
+
+            while (true)
+            {
+                //dq = LoadControl(); // loading a Dynamic Data Field page *.ascx
+                dq.ID = string.Concat("dq", "database data");
+                dynamicControls.Controls.Add(dq);
+                dynamicControls.Controls.Add(new LiteralControl("<br /><br />"));
+            }
+
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Make sure you add the controls outside of the PostBack below, otherwise any values set 
+            // will not be persisted in the viewstate and lost during postback
+
+            if (!Page.IsPostBack)
+            {
+                /* If inititialize dynamic controls here the controls will disappear during postbacks */
+                // Now you can find and set the controls here
+
+                lbl.Text = DateTime.Now.ToString();
+
+                // At some point later in the page lifecycle reference the dynamic TextBox
+                TextBox dTB = (TextBox)placeHolder1.FindControl("dynTextBox");
+                dTB.Text = "FoundYou";
+                dTB.BackColor = System.Drawing.Color.Red; // Initial BackColor
+
+                Button dBtn1 = (Button)placeHolder1.FindControl("dynButton1");
+                dBtn1.Text = "Click Me1";
+
+                Button dBtn2 = (Button)placeHolder1.FindControl("dynButton2");
+                dBtn2.Text = "Click Me2";
+                // it is important to have a unique ID for each control, this can be pulled out of the database via the table's unique primary key field
+            }
+
+            btn.Click += new EventHandler(ButtonClickEventHandler);
+        }
+
+        public static void ButtonClickEventHandler(Object sender, EventArgs e)
+        {
+            //TextBox clkTB = (TextBox)placeHolder1.FindControl("dynTextBox");
+            // Do something
+        }
+        public static void RecurseThroughControlHierarchy(Control c)
+        {
+            // Do whatever you want with this control
+            // Recurse through c's children controls
+            // This works when you have a number of similar controls
+
+            foreach (Control child in c.Controls)
+            {
+                RecurseThroughControlHierarchy(child);
+            }
+        }
+
+    }
+}
