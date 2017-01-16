b45e835c2ce61d4526d6709238977d245f0998fe
jean-raymond <jpierre@live.com>
2017-01-15 22:32:06 -0800

    more dynamic controls

--- latus/latus/dyncontrol.aspx.cs
+++ latus/latus/dyncontrol.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
 
 namespace latus
 {
@@ -12,6 +13,8 @@ namespace latus
         Label lbl = new Label();
         TextBox tb = new TextBox();
         Button btn = new Button();
+        GridView gv = new GridView();
+
         protected void Page_Init(object sender, EventArgs e)
         {
             // Add dynamic controls to the init portion of the web page
@@ -25,16 +28,19 @@ namespace latus
             dynControl.Controls.Add(btn);
             btn.ID = "dynButton1";
 
+            dynControl.Controls.Add(gv);
+            gv.ID = "dynGridView";
 
-            UserControl dq = new UserControl();
 
-            while (true)
-            {
-                //dq = LoadControl(); // loading a Dynamic Data Field page *.ascx
-                dq.ID = string.Concat("dq", "database data");
-                dynControl.Controls.Add(dq);
-                dynControl.Controls.Add(new LiteralControl("<br /><br />"));
-            }
+            //UserControl dq = new UserControl();
+
+            //while (true)
+            //{
+            //    //dq = LoadControl(); // loading a Dynamic Data Field page *.ascx
+            //    dq.ID = string.Concat("dq", "database data");
+            //    dynControl.Controls.Add(dq);
+            //    dynControl.Controls.Add(new LiteralControl("<br /><br />"));
+            //}
 
         }
         protected void Page_Load(object sender, EventArgs e)
@@ -56,6 +62,13 @@ namespace latus
 
                 Button dBtn1 = (Button)dynControl.FindControl("dynButton1");
                 dBtn1.Text = "Click Me";
+
+                GridView dGv = (GridView)dynControl.FindControl("dynGridView");
+                dGv.DataSource = _sampleData;
+                dGv.DataBind();
+                //dGv.GridLines = "Vertical";
+                //dGv.AutoGenerateColumns = false;
+                //dGv.Columns.Add((TextBox)
 
                 // it is important to have a unique ID for each control, this can be pulled out of the database via the table's unique primary key field
             }
@@ -80,5 +93,43 @@ namespace latus
             }
         }
 
+        private DataTable _sampleData
+        {
+            get
+            {
+                DataTable dt = (DataTable)Session["TestData"];
+
+                if (dt == null)
+                {
+                    // Create a DataTable and save it to session
+                    dt = new DataTable();
+
+                    dt.Columns.Add(new DataColumn("Id", typeof(int)));
+                    dt.Columns.Add(new DataColumn("Description", typeof(string)));
+                    dt.Columns.Add(new DataColumn("AssignedTo", typeof(string)));
+                    dt.Columns.Add(new DataColumn("Status", typeof(string)));
+                    dt.Columns.Add(new DataColumn("Tick", typeof(string)));
+
+                    dt.Rows.Add(new object[] { 1, "Create a new project", "Declan", "Complete", true });
+                    dt.Rows.Add(new object[] { 2, "Build a demo applcation", "Olive", "In Progress", false });
+                    dt.Rows.Add(new object[] { 3, "Test the demo applcation", "Peter", "Pending", true });
+                    dt.Rows.Add(new object[] { 4, "Deploy the demo applcation", "Andy", "Pending", false });
+                    dt.Rows.Add(new object[] { 5, "Support the demo applcation", "", "Pending", true });
+
+                    // Add the id column as a primary key
+                    DataColumn[] keys = new DataColumn[1];
+                    keys[0] = dt.Columns["id"];
+                    dt.PrimaryKey = keys;
+
+                    _sampleData = dt;
+                }
+
+                return dt;
+            }
+            set
+            {
+                Session["TestData"] = value;
+            }
+        }
     }
 }
